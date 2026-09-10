912790e6bc67c732798b8ef9abc8690bd92274c2
stark732 <106365909+stark732@users.noreply.github.com>
2026-09-10 23:53:56 +0530

    Time: 4 ms (36.43%) | Memory: 14.3 MB (38.90%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void solve(vector<int>& candidates, int target, int i, vector<int>& temp) {
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if (i == candidates.size() || target < 0) {
+            return;
+        }
+
+        // Take candidates[i]
+        if (candidates[i] <= target) {
+            temp.push_back(candidates[i]);
+
+            // Same index because we can use the number again
+            solve(candidates, target - candidates[i], i, temp);
+
+            temp.pop_back();
+        }
+
+        // Don't take candidates[i]
+        solve(candidates, target, i + 1, temp);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> temp;
+        solve(candidates, target, 0, temp);
+        return ans;
+    }
+};
